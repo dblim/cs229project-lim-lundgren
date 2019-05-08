0b987285fe7c7ee651f3a753777f23d725bbc857
julu123 <julu1@stanford.edu>
2019-05-08 11:28:35 -0700

    Update alpha_vantage_test.py

--- src/alpha_vantage_test.py
+++ src/alpha_vantage_test.py
@@ -1,5 +1,8 @@
 from alpha_vantage.timeseries import TimeSeries
-ts = TimeSeries(key='YOUR_API_KEY')
-data, _ = ts.get_intraday('GOOGL')
+import pandas as pd
+import matplotlib.pyplot as plt
+ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+data, _ = ts.get_intraday('GOOGL', outputsize='full')
 
-print(data)
+data['4. close'].plot()
+plt.show()
