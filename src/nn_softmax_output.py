e7b1e9fe242484130fca5cac71def4b016078bec
David Benjamin Lim <benjamin.colim@gmail.com>
2019-05-20 21:19:16 -0700

    Added regularization

--- src/nn_softmax_output.py
+++ src/nn_softmax_output.py
@@ -65,6 +65,9 @@ n_val, _ = x_val.shape
 learning_rate = 0.001
 training_epochs = 5000
 
+# Regularization parameters
+beta = 0.1
+
 # Arrays:
 W1_weights: np.ndarray
 b1_weights: np.ndarray
@@ -108,6 +111,8 @@ if t_flow is True:
     # Loss function
     y_clipped = tf.clip_by_value(y_hat, 1e-10, 0.9999999)
     cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=y_clipped))
+    #regularizer = beta*(tf.nn.l2_loss(W1)   + tf.nn.l2_loss(W2)  + tf.nn.l2_loss(W1))
+    #cost = tf.reduce_mean(cost + beta * regularizer)
     prediction = tf.round(y_hat)
     correct = tf.cast(tf.equal(prediction, y), dtype=tf.float32)
     accuracy = tf.reduce_mean(correct)
