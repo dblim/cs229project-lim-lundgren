536800a0f84416f6f9f96bbcd6fe15d35592f57c
David Benjamin Lim <benjamin.colim@gmail.com>
2019-06-03 22:27:18 -0700

    update ARIMA

--- src/ARIMA/ARIMA.py
+++ src/ARIMA/ARIMA.py
@@ -1,8 +1,83 @@
 from statsmodels.tsa.statespace.varmax import VARMAX
-from utils import combine_ts, minutizer, preprocess_2
+#from utils import combine_ts, minutizer, preprocess_2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+def combine_ts(tickers: list):
+    stock0 = tickers[0]
+    path = '../data/sectors/Information-Technology/'+stock0+'.csv'
+    data = pd.read_csv(path, index_col="timestamp", parse_dates=True)
+    renamer = {'close': stock0+'_close', 'high': stock0+'_high', 'low': stock0+'_low',
+               'open': stock0+'_open', 'volume': stock0+'_volume', }
+    data = data.rename(columns=renamer)
+    tickers.remove(tickers[0])
+    for str in tickers:
+        path = '../data/sectors/Information-Technology/'+str+'.csv'
+        new_data = pd.read_csv(path, index_col="timestamp", parse_dates=True)
+        renamer = {'close': str+'_close', 'high': str+'_high', 'low': str+'_low',
+                   'open': str + '_open', 'volume': str+'_volume', }
+        new_data = new_data.rename(columns=renamer)
+
+        data = pd.concat([data, new_data], axis=1, sort=True)
+    tickers.insert(0, stock0)
+    return data.interpolate()[1:data.shape[0]]
+
+
+def minutizer(data, split: int = 5, ground_features: int = 5):
+    n, d = data.shape
+    new_data = pd.DataFrame(np.zeros((int(n/split) - 1, d)), columns=list(data))
+    for i in range(int(n/split) - 1):
+        for j in range(int(d/ground_features)):
+            # Close
+            new_data.iloc[i, j * ground_features] = data.iloc[split * (i + 1), j * ground_features]
+            # High
+            new_data.iloc[i, j * ground_features + 1] = max([data.iloc[split*i+k, j * ground_features + 1]
+                                                             for k in range(split)])
+            # Low
+            new_data.iloc[i, j * ground_features + 2] = min([data.iloc[split * i + k, j * ground_features + 2]
+                                                             for k in range(split)])
+            # Open
+            new_data.iloc[i, j * ground_features + 3] = data.iloc[split*i, j * ground_features + 3]
+            # Volume
+            new_data.iloc[i, j * ground_features + 4] = np.sum(data.iloc[i*split:(i+1)*split, j * ground_features + 4])
+    return new_data
+
+
+def preprocess_2(data, tickers, ground_features: int = 5, new_features: int = 5):
+    n, d = data.shape
+    new_d = int(d/ground_features)
+    new_data = np.zeros((n, new_d * new_features))
+    open_prices = np.zeros((n, new_d))
+    for i in range(new_d):
+        new_data[:, new_features * i] = \
+            data.iloc[:, ground_features * i]/data.iloc[:, ground_features * i + 3] - 1  # Returns
+        new_data[:, new_features * i + 1] = \
+            data.iloc[:, ground_features * i + 1] - data.iloc[:, ground_features * i + 2]  # Spread
+        new_data[:, new_features * i + 2] = \
+            data.iloc[:, ground_features * i + 4] - np.mean(data.iloc[:, ground_features * i + 4])# Volume
+        new_data[:, new_features * i + 3] = \
+            data.iloc[:, ground_features * i + 3] - np.mean(data.iloc[:, ground_features * i + 3])  # Open
+        # Laguerre polynomials
+        """
+        new_data[:, new_features * i + 3] = np.exp(- 0.5 * new_data[:, new_features * i + 3])
+        new_data[:, new_features * i + 4] = new_data[:, new_features * i + 4] * (1 - new_data[:, new_features * i + 3])
+        new_data[:, new_features * i + 5] = new_data[:, new_features * i + 4] * \
+            (1 - 2 * new_data[:, new_features * i + 3] + 0.5 * np.square(new_data[:, new_features * i + 3]))
+        """
+        new_data[:, new_features * i + 4] = \
+            np.sin(2 * np.pi * new_data[:, new_features * i + 3]/np.max(new_data[:, new_features * i + 3]))  # Sin
+
+        open_prices[:, i] = data.iloc[:, ground_features * i + 3]
+    header_data = []
+    for ticker in tickers:
+        header_data.append(ticker + '_returns')  # 0
+        header_data.append(ticker + '_spread')  # 1
+        header_data.append(ticker + '_volume')  # 2
+        header_data.append(ticker + '_open')  # 3
+        header_data.append(ticker + '_sin_open')  # 8
+    return pd.DataFrame(new_data, columns=header_data)
 
 tickers = ['ACN', 'AMAT', 'CDNS', 'IBM', 'INTU', 'LRCX', 'NTAP', 'VRSN', 'WU', 'XLNX']
 
