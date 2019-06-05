a3a90533f100afd6956a15a27d469a5b1508baf4
David Benjamin Lim <benjamin.colim@gmail.com>
2019-06-04 17:38:19 -0700

    update

--- src/LSTM/lstm_single.py
+++ src/LSTM/lstm_single.py
@@ -4,7 +4,56 @@ from keras import optimizers
 import numpy as np
 import pandas as pd
 from pandas import read_csv
-from utils import minutizer, preprocess_2_single
+#from utils import minutizer, preprocess_2_single
+
+
+def minutizer(data, split: int = 5, ground_features: int = 5):
+    n, d = data.shape
+    new_data = pd.DataFrame(np.zeros((int(n/split) - 1, d)), columns=list(data))
+    for i in range(int(n/split) - 1):
+        for j in range(int(d/ground_features)):
+            # Close
+            new_data.iloc[i, j * ground_features] = data.iloc[split * (i + 1), j * ground_features]
+            # High
+            new_data.iloc[i, j * ground_features + 1] = max([data.iloc[split*i+k, j * ground_features + 1]
+                                                             for k in range(split)])
+            # Low
+            new_data.iloc[i, j * ground_features + 2] = min([data.iloc[split * i + k, j * ground_features + 2]
+                                                             for k in range(split)])
+            # Open
+            new_data.iloc[i, j * ground_features + 3] = data.iloc[split*i, j * ground_features + 3]
+            # Volume
+            new_data.iloc[i, j * ground_features + 4] = np.sum(data.iloc[i*split:(i+1)*split, j * ground_features + 4])
+    return new_data
+
+
+def preprocess_2_single(data, ticker: str, ground_features: int = 5, new_features: int = 5):
+    n, d = data.shape
+    new_d = int(d/ground_features)
+    new_data = np.zeros((n, new_d * new_features))
+    open_prices = np.zeros((n, new_d))
+    for i in range(new_d):
+        new_data[:, new_features * i] = \
+            data.iloc[:, ground_features * i]/data.iloc[:, ground_features * i + 3] - 1  # Returns
+        new_data[:, new_features * i + 1] = \
+            data.iloc[:, ground_features * i + 1] - data.iloc[:, ground_features * i + 2]  # Spread
+        new_data[:, new_features * i + 2] = \
+            data.iloc[:, ground_features * i + 4]  # Volume
+        new_data[:, new_features * i + 3] = \
+            data.iloc[:, ground_features * i + 3]  # Open price
+        new_data[:, new_features * i + 4] = \
+            np.sin(2 * np.pi * new_data[:, new_features * i + 3]/np.max(new_data[:, new_features * i + 3]))  # Sin
+
+        open_prices[:, i] = data.iloc[:, ground_features * i + 3]
+    header_data = []
+    header_open = []
+    header_data.append(ticker + '_returns')
+    header_data.append(ticker + '_spread')
+    header_data.append(ticker + '_volume')  # Normalized
+    header_data.append(ticker + '_normalized_open')
+    header_data.append(ticker + '_sin_returns')
+    header_open.append(ticker + '_open')
+    return pd.DataFrame(new_data, columns=header_data), pd.DataFrame(open_prices, columns=header_open)
 
 
 def lstm_model(stock: str,
