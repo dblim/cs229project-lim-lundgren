3e31d21e4c78461de5947a4274518c92107803dd
David Benjamin Lim <benjamin.colim@gmail.com>
2019-06-07 14:44:31 -0700

    update

--- src/LSTM/change_tuning.py
+++ src/LSTM/change_tuning.py
@@ -5,22 +5,11 @@ from keras import optimizers
 import numpy as np
 import pandas as pd
 import random
-from lstm_utils import minutizer, combine_ts, preprocess_2_multi
+from lstm_utils import minutizer, combine_ts, preprocess_2_multi, customized_loss
 
-
-def customized_loss(y_pred, y_true):
-    num = K.sum(K.square(y_pred - y_true), axis=-1)
-    y_true_sign = y_true > 0
-    y_pred_sign = y_pred > 0
-    logicals = K.equal(y_true_sign, y_pred_sign)
-    logicals_0_1 = K.cast(logicals, 'float32')
-    den = K.sum(logicals_0_1, axis=-1)
-    return num/(1 + den)
-
-
-def lstm_model_mse(lstm_units :list, batch_size : list, stocks: list,
+def lstm_model_mse(lstm_units :int, batch_size : int, stocks: list,
                lookback: int = 24,
-               epochs: int = 50,
+               epochs: int = 2,
                learning_rate: float = 0.0001,
                dropout_rate: float = 0.1,
                ground_features: int = 4,
@@ -59,60 +48,56 @@ def lstm_model_mse(lstm_units :list, batch_size : list, stocks: list,
     y_test = Y[int(new_n * train_val_test_split['val']): int(new_n * train_val_test_split['test'])]
 
     # Hyperparameter printing
-    for units_num in lstm_units:
-        for batch_num in batch_size:
-
-            # Initialising the LSTM
-            model = Sequential()
-
-            # Adding layers. LSTM(n) --> Dropout(p)
-            model.add(LSTM(units=units_num, return_sequences=True, use_bias=True, input_shape=(lookback, ground_features)))
-            model.add(Dropout(dropout_rate))
-
-            model.add(LSTM(units=10, use_bias=False))
-            model.add(Dropout(dropout_rate))
-
-            # Output layer
-            model.add(Dense(units=1, activation='linear', use_bias=True))
-
-            # Optimizer
-            adam_opt = optimizers.adam(lr=learning_rate)
-
-            # Compile
-            model.compile(optimizer=adam_opt, loss=customized_loss)
-
-            # Fit
-            history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_num, validation_data=(X_val, y_val))
-
-            # Validate
-            predicted_stock_returns = model.predict(X_val)
-
-            for i, ticker in enumerate(stocks):
-                predcted_returns = np.zeros((int(y_val.shape[0] / amount_of_stocks), 1))
-                actual_returns = np.zeros((int(y_val.shape[0] / amount_of_stocks), 1))
-                for j in range(int(y_val.shape[0] / amount_of_stocks)):
-                    predcted_returns[j] = predicted_stock_returns[amount_of_stocks * j + i]
-                    actual_returns[j] = y_val[amount_of_stocks * j + i]
-                #
-                MSE = sum((predcted_returns - actual_returns) ** 2) / y_val.shape[0]
-
-            print('MSE:', MSE)
-            print('Number of LSTM cells:', units_num)
-            print('Batch size:', batch_num)
 
 
-# Search for lstm_units
+    # Initialising the LSTM
+    model = Sequential()
 
-# 10 numbers sampled  randomly between 100 and 800
-lstm_range = [i for i in range(10,50)]
-lstm_units = random.sample(lstm_range, 5)
+    # Adding layers. LSTM(n) --> Dropout(p)
+    model.add(LSTM(units=lstm_units, return_sequences=True, use_bias=True, input_shape=(lookback, ground_features)))
+    model.add(Dropout(dropout_rate))
 
-# Search for batch size. Original chose was 96
-# 10 numbers sampled  randomly between 50 and 100
+    model.add(LSTM(units=10, use_bias=False))
+    model.add(Dropout(dropout_rate))
 
-batch_size_range = [i for i in range(90,110)]
-batch_size = random.sample(batch_size_range, 5)
+    # Output layer
+    model.add(Dense(units=1, activation='linear', use_bias=True))
 
-tickers = ['ACN', 'AMAT' , 'CDNS', 'IBM', 'INTU', 'LRCX', 'NTAP', 'VRSN', 'WU', 'XLNX']
+    # Optimizer
+    adam_opt = optimizers.adam(lr=learning_rate)
 
-lstm_model_mse(lstm_units, batch_size,tickers)
+    # Compile
+    model.compile(optimizer=adam_opt, loss=customized_loss)
+
+    # Fit
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_val, y_val), verbose =0)
+
+    # Validate
+    predicted_stock_returns = model.predict(X_val)
+
+    all_mse = []
+    for i, ticker in enumerate(stocks):
+        predcted_returns = np.zeros((int(y_val.shape[0] / amount_of_stocks), 1))
+        actual_returns = np.zeros((int(y_val.shape[0] / amount_of_stocks), 1))
+        for j in range(int(y_val.shape[0] / amount_of_stocks)):
+            predcted_returns[j] = predicted_stock_returns[amount_of_stocks * j + i]
+            actual_returns[j] = y_val[amount_of_stocks * j + i]
+        #
+        MSE = sum((predcted_returns - actual_returns) ** 2) / y_val.shape[0]
+        all_mse.append(MSE)
+
+    avg_mse = np.array(all_mse)
+    print('Average MSE:', np.mean(avg_mse))
+
+tickers = ['ACN', 'AMAT' ,  'CDNS', 'IBM', 'INTU', 'LRCX', 'NTAP', 'VRSN', 'WU', 'XLNX']
+
+# Search lstm size between 10 and 50
+# Search batch size between 50 and 100.
+random.seed()
+
+#Choose 10 random random numbers
+for k in range(10):
+    lstm_units, batch_size = random.randint(10, 50), random.randint(80, 120)
+    print('Number of LSTM cells:', lstm_units)
+    print('Batch size:', batch_size)
+    lstm_model_mse(lstm_units, batch_size, tickers)
--- src/LSTM/lstm_utils.py
+++ src/LSTM/lstm_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import keras.backend as K
 
 def minutizer(data, split: int = 5, ground_features: int = 5):
     n, d = data.shape
@@ -79,8 +80,6 @@ def preprocess_2_single(data, ticker: str, ground_features: int = 5, new_feature
     header_open.append(ticker + '_open')
     return pd.DataFrame(new_data, columns=header_data), pd.DataFrame(open_prices, columns=header_open)
 
-
-
 def combine_ts(tickers: list):
     stock0 = tickers[0]
     path = '../data/sectors/Information Technology/'+stock0+'.csv'
@@ -99,5 +98,15 @@ def combine_ts(tickers: list):
         data = pd.concat([data, new_data], axis=1, sort=True)
     tickers.insert(0, stock0)
     return data.interpolate()[1:data.shape[0]]
+
+def customized_loss(y_pred, y_true):
+    num = K.sum(K.square(y_pred - y_true), axis=-1)
+    y_true_sign = y_true > 0
+    y_pred_sign = y_pred > 0
+    logicals = K.equal(y_true_sign, y_pred_sign)
+    logicals_0_1 = K.cast(logicals, 'float32')
+    den = K.sum(logicals_0_1, axis=-1)
+    return num/(1 + den)
+
 
 
