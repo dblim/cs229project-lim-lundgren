24b01250d789e9f66d33bdd83cd804c3b0be921f
David Benjamin Lim <benjamin.colim@gmail.com>
2019-06-08 14:19:27 -0700

    u

--- src/LSTM/hyp_tuning.py
+++ src/LSTM/hyp_tuning.py
@@ -136,9 +136,9 @@ if rand_tuning is True:
     avg_mse_list = []
 
     # Choose 2 random pairs of numbers for lstm units, lookback, dropout rate
-    num_trials = 15
+    num_trials = 2
     for k in range(num_trials):
-        lookback, learnin_rate, output_dim_individual_layer =  \
+        lookback, learning_rate, output_dim_individual_layer =  \
         random.randint(10, 40), np.random.choice([0.1, 0.01, 0.001, 0.0001, 0.5, 0.05, 0.005, 0.0005]), random.randint( 4, 40)
 
         avg_mse = lstm_model_mse( lookback, learning_rate, output_dim_individual_layer, tickers)
