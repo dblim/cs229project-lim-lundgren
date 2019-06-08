a20e1812b90ade2cfb8c7b2737287798b311d18d
julu123 <julu1@stanford.edu>
2019-06-07 21:53:40 -0700

    -

--- src/LSTM/validation_investigation.py
+++ src/LSTM/validation_investigation.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+tickers = ['ACN', 'AMAT', 'CDNS', 'IBM', 'INTU', 'LRCX', 'NTAP', 'VRSN', 'WU', 'XLNX']
 ticker = 'IBM'
 
-pred_path = '../output/LSTM_results/single_valid/'+ticker+''
+for ticker in tickers:
+
+    pred_path = '../output/LSTM_results/single_valid/'+ticker+'_val_predictions.csv'
+    real_path = '../output/LSTM_results/single_valid/'+ticker+'_val_real.csv'
+
+    pred = pd.read_csv(pred_path).values
+    real = pd.read_csv(real_path).values
+
+    plt.hist(real, bins=100, density=True)
+    plt.hist(pred,  bins=100, density=True, alpha=0.75)
+    plt.axis([-0.01, 0.01, 0, 1000])
+    plt.show()
